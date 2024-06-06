8f62a050d7c583b0144c624fb53dba426700d813
66596 <Kevin.Putra@bni.co.id>
2024-06-06 15:01:16 +0700

    refactor: change max num of news to 10

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,7 +62,7 @@ if company:
     news_text = []
     
     query = f'{company} after:2023-01-01'
-    response = google_search(query, API_KEY, SEARCH_ENGINE_ID, num=50)
+    response = google_search(query, API_KEY, SEARCH_ENGINE_ID, num=10)
 
     url_collection = [item['link'] for item in response]
     import os
